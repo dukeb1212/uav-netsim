c4b55cb091dd7c1c70e7f33bf068478afe8974e2
dukeb1212 <godbsea5@gmail.com>
2024-11-19 00:18:44 +0700

    fix: PX4 script

--- px4_script.sh
+++ px4_script.sh
@@ -8,15 +8,15 @@ cd /src/PX4-Autopilot/
 
 make px4_sitl none_iris > /dev/null 2>&1 &
 
-while true; do
-  if pgrep -x "px4-simulator" > /dev/null; then
-    ps -ef | grep -E 'px4|px4-simulator' | grep -v grep | awk '{print $2}' | xargs kill -9
-       break
-  fi
-  sleep 1
-done
+#while true; do
+#  if pgrep -x "px4-simulator" > /dev/null; then
+#    ps -ef | grep -E 'px4|px4-simulator' | grep -v grep | awk '{print $2}' | xargs kill -9
+#       break
+#  fi
+#  sleep 1
+#done
 
-./Tools/sitl_multiple_run.sh "$count"
+#./Tools/sitl_multiple_run.sh "$count"
 
 sleep infinity
 
