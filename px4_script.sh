8a18201719a5ebda1e05c8ccf84615b987fef533
dukeb1212 <godbsea5@gmail.com>
2024-11-19 00:39:27 +0700

    fix: startup script

--- px4_script.sh
+++ px4_script.sh
@@ -2,21 +2,19 @@
 
 count="$1"
 
-git config --global --add safe.directory /PX4-Autopilot
-
 cd /src/PX4-Autopilot/
 
 make px4_sitl none_iris > /dev/null 2>&1 &
 
-#while true; do
-#  if pgrep -x "px4-simulator" > /dev/null; then
-#    ps -ef | grep -E 'px4|px4-simulator' | grep -v grep | awk '{print $2}' | xargs kill -9
-#       break
-#  fi
-#  sleep 1
-#done
+while true; do
+  if pgrep -x "px4-simulator" > /dev/null; then
+    ps -ef | grep -E 'px4|px4-simulator' | grep -v grep | awk '{print $2}' | xargs kill -9
+       break
+  fi
+  sleep 1
+done
 
-#./Tools/sitl_multiple_run.sh "$count"
+./Tools/sitl_multiple_run.sh "$count"
 
 sleep infinity
 
